7ce240873822d26be326c0d2d51a834fead38e08
Arman <armanurstem@gmail.com>
2024-05-20 16:24:10 +0500

    Initial Commit

---
+++ Arbuz_HW/Arbuz_HWApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arbuz_HWApp.swift
+//  Arbuz_HW
+//
+//  Created by Arman  Urstem on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Arbuz_HWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
