8598adfb49bc31cfc3751427a85ee9e47e76caa4
Arman <armanurstem@gmail.com>
2024-05-20 18:10:00 +0500

    ViewModels and changing issues in somewhere

---
+++ Arbuz_HW/ViewModels/ParserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ParserViewModel.swift
+//  Arbuz_HW
+//
+//  Created by Arman  Urstem on 20.05.2024.
+//
+
+import Foundation
---
+++ Arbuz_HW/ViewModels/ProductDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Arbuz_HW
+//
+//  Created by Arman  Urstem on 20.05.2024.
+//
+
+import Foundation
