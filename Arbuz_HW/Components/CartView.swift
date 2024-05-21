84bf0a660f93f42c0d7708d009f6e81c3c9d1740
Arman <armanurstem@gmail.com>
2024-05-21 15:00:57 +0500

    change CartView

--- Arbuz_HW/Components/CartView.swift
+++ Arbuz_HW/Components/CartView.swift
@@ -1,10 +1,3 @@
-//
-//  CartView.swift
-//  Arbuz_HW
-//
-//  Created by Arman  Urstem on 20.05.2024.
-//
-
 import SwiftUI
 
 struct CartView: View {
@@ -24,22 +17,50 @@ struct CartView: View {
                     Spacer()
                 }
                 .frame(width: 250)
-            }
-            else {
+            } else {
                 ScrollView {
                     VStack(spacing: 20) {
                         VStack(spacing: 8) {
                             ForEach(viewModel.positions, id: \.id) { position in
                                 PositionCell(viewModel: CartViewModel.shared, count: position.count, position: position)
-                              
                             }
                         }
+                        
+                        if viewModel.cost < 8000 {
+                            Text("Бесплатная доставка от 8000")
+                                .foregroundColor(.secondary)
+                                .font(.system(size: 14))
+                                .fontWeight(.medium)
+                                .padding([.horizontal, .top], 8)
+                            
+                        }
+                        
                         VStack(spacing: 20) {
-                            HStack {
-                                Text("Итого")
-                                Spacer()
-                                Text("\(self.viewModel.cost)")
+                            VStack(spacing: 20) {
+                                HStack {
+                                    Text("Итого")
+                                    Spacer()
+                                    Text("\(viewModel.cost) тг")
+                                }
+                                
+                                HStack {
+                                    Text("Доставка")
+                                    Spacer()
+                                    Text(viewModel.cost < 8000 ? "1000 тг" : "Бесплатно")
+                                }
+                                
+                                HStack {
+                                    Text("Общая сумма")
+                                    Spacer()
+                                    Text("\(viewModel.cost + (viewModel.cost < 8000 ? 1000 : 0)) тг")
+                                }
                             }
+                            .padding()
+                            .background(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color(Colors.lightGray.rawValue))
+                            )
+                            
                             HStack(spacing: 8) {
                                 Button {
                                     viewModel.positions = []
